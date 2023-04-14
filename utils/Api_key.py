98b3f234e96733872f065a6fc67c78df587b4533
Lib0823 <robot082373@naver.com>
2023-04-14 07:27:06 +0000

    chore: debuging

--- utils/Api_key.py
+++ utils/Api_key.py
@@ -1,8 +1,8 @@
 
 # openAI - API KEY
-openAI_key = "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"
+openAI_key = ""
 
 
 # 기상청 - API KEY
-weather_key = "9OP4JjJ3rCjasruBrK2W9LfumFlvsZrGkQzsRCbOxBf7N2sauNdq/NqeB7Ks0lCXYAa75lqua4j6HGLON0ExAw=="
+weather_key = ""
 
