817f1aca64b1eaecd62c4e66f1732f6bb44a5b5a
inbeom Lee <robor082373@gmail.com>
2023-05-09 18:10:58 +0000

    chorn: api key update

--- utils/Api_key.py
+++ utils/Api_key.py
@@ -1,8 +1,8 @@
 
 # openAI - API KEY
-openAI_key = "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"
+openAI_key = ""
 
 
 # 기상청 - API KEY
-weather_key = "9OP4JjJ3rCjasruBrK2W9LfumFlvsZrGkQzsRCbOxBf7N2sauNdq/NqeB7Ks0lCXYAa75lqua4j6HGLON0ExAw=="
+weather_key = ""
 
